e505162b69b7ca82337a78421e3e0daea7fa5976
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2022-01-05 21:15:36 -0300

    criado index.ts do components.

--- src/app/conversor/index.ts
+++ src/app/conversor/index.ts
@@ -1,2 +1,3 @@
 export * from './conversor.module'; //Add export do conversor.module
 export * from './models'; //Add export da pasta models
+export * from './components'; //Add export da pasta components
